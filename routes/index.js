d7d2f8b39ade3d678ae8adc1ffa81fa06b7a6744
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-01-21 16:01:04 -0800

    home page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Wolfbergr' });
 });
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    //optionally force pick account every time
+    prompt: 'select_account',
+  }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies',
+  } 
+));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/movies')
+});
+
 
 module.exports = router;
